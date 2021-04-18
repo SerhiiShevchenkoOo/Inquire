b513ad56389946625f2cf0bf95420d6874899aea
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2021-04-18 12:58:25 +0300

    add root style material ui

---
+++ src/style/index.ts
@@ -0,0 +1,9 @@
+/* eslint import/prefer-default-export: 0 */
+// material
+import { createMuiTheme } from '@material-ui/core/styles'
+
+export const rootStyle = createMuiTheme({
+	typography: {
+		htmlFontSize: 10,
+	},
+})
